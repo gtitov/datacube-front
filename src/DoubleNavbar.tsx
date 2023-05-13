976293cf714c98baa159fc6d14b69d7096acc490
gtitov <gherman.s.titov@gmail.com>
2023-05-13 22:02:32 +0300

    second day: map

--- src/DoubleNavbar.tsx
+++ src/DoubleNavbar.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
-import { createStyles, Navbar, UnstyledButton, Tooltip, Title, Select, rem } from '@mantine/core';
+import DeckGL from '@deck.gl/react/typed';
+import { H3HexagonLayer } from '@deck.gl/geo-layers/typed';
+import { Map } from 'react-map-gl';
+import { createStyles, AppShell, Navbar, Header, UnstyledButton, Tooltip, Title, Select, MultiSelect, Switch, Divider, rem } from '@mantine/core';
+
 import {
   IconEyeglass,
   IconDeviceDesktopAnalytics,
@@ -17,9 +21,8 @@ const useStyles = createStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    borderRight: `${rem(1)} solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
-    }`,
+    borderRight: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
+      }`,
   },
 
   main: {
@@ -56,9 +59,8 @@ const useStyles = createStyles((theme) => ({
     padding: theme.spacing.md,
     paddingTop: rem(18),
     height: rem(60),
-    borderBottom: `${rem(1)} solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
-    }`,
+    borderBottom: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
+      }`,
   },
 
   logo: {
@@ -68,9 +70,8 @@ const useStyles = createStyles((theme) => ({
     justifyContent: 'center',
     height: rem(60),
     paddingTop: theme.spacing.md,
-    borderBottom: `${rem(1)} solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
-    }`,
+    borderBottom: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
+      }`,
     marginBottom: theme.spacing.xl,
   },
 
@@ -115,6 +116,15 @@ const variablesAvaliable = ['o2', 'spco2', 'chl'];
 const datesAvaliable = [...Array(12)].map((v, i) => `2020-${String(i + 1).padStart(2, '0')}-01`);
 const depthsAvaliable = ['0', '2', '5'];
 
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: 40.41669,
+  latitude: 80.7853,
+  zoom: 2,
+  pitch: 0,
+  bearing: 0,
+};
+
 export function DoubleNavbar() {
   const { classes, cx } = useStyles();
   const [activeMainLink, setActiveMainLink] = useState('Просмотр');
@@ -152,7 +162,7 @@ export function DoubleNavbar() {
       <UnstyledButton
         onClick={() => setActiveMainLink(link.label)}
         className={
-            cx(classes.mainLink, { [classes.mainLinkActive]: link.label === activeMainLink })
+          cx(classes.mainLink, { [classes.mainLinkActive]: link.label === activeMainLink })
         }
       >
         <link.icon size="1.4rem" stroke={1.5} />
@@ -175,35 +185,104 @@ export function DoubleNavbar() {
   // ));
 
   return (
-    <Navbar height="100vh" width={{ sm: 500 }}>
-      <Navbar.Section grow className={classes.wrapper}>
-        <div className={classes.aside}>
-          <div className={classes.logo}>
-            {/* <MantineLogo type="mark" size={30} /> */}
-          </div>
-          {mainLinks}
-        </div>
-        <div className={classes.main}>
-          <Title order={4} className={classes.title}>
-            {activeMainLink}
-          </Title>
-          <Select
-            data={variablesAvaliable}
-            value={selectedVariable}
-            onChange={setSelectedVariable}
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar height="100vh" width={{ sm: 500 }}>{
+          <Navbar.Section grow className={classes.wrapper}>
+            <div className={classes.aside}>
+              <div className={classes.logo}>
+                {/* <MantineLogo type="mark" size={30} /> */}
+              </div>
+              {mainLinks}
+            </div>
+            <div className={classes.main}>
+              <Title order={4} className={classes.title}>
+                {activeMainLink}
+              </Title>
+              <Title m="md" order={6}>
+                Параметры среды
+              </Title>
+              <Select
+                m="md"
+                data={variablesAvaliable}
+                value={selectedVariable}
+                onChange={setSelectedVariable}
+              />
+              <Select
+                m="md"
+                data={datesAvaliable}
+                value={selectedDate}
+                onChange={setSelectedDate}
+              />
+              <Select
+                m="md"
+                data={depthsAvaliable}
+                value={selectedDepth}
+                onChange={setSelectedDepth}
+              />
+
+              <Divider my="xl" />
+              <Title m="md" order={6}>
+                Животные
+              </Title>
+
+              <Switch
+                m="md"
+                labelPosition="left"
+                label="Сгруппировать по родам"
+              />
+              <MultiSelect
+                m="md"
+                data={depthsAvaliable}
+              />
+            </div>
+          </Navbar.Section>
+        }
+        </Navbar>
+      }
+      header={<Header height={60} p="xs">{/* Header content */}</Header>}
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+    >
+      {
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller
+        >
+          {/* <Map mapboxAccessToken="pk.eyJ1IjoiZ2hlcm1hbnQiLCJhIjoiY2pncDUwcnRmNDQ4ZjJ4czdjZXMzaHZpNyJ9.3rFyYRRtvLUngHm027HZ7A" /> */}
+          <H3HexagonLayer
+            id="hexagons-context"
+            data={mapData}
+            filled
+            pickable
+            getHexagon={e => e.h3}
+            getFillColor={e => [0, e[selectedVariable] * 10 - 100, 200]}
           />
-          <Select
-            data={datesAvaliable}
-            value={selectedDate}
-            onChange={setSelectedDate}
-          />
-          <Select
-            data={depthsAvaliable}
-            value={selectedDepth}
-            onChange={setSelectedDepth}
-          />
-        </div>
-      </Navbar.Section>
-    </Navbar>
+        </DeckGL>
+        // <DeckGL
+        //   controller
+        //   initialViewState={{
+        //     longitude: -122.41669,
+        //     latitude: 37.7853,
+        //     zoom: 2,
+        //     pitch: 0,
+        //     bearing: 0,
+        //   }}
+        // >
+        //   {/* <H3HexagonLayer
+        //   id="hexagons"
+        //   data={mapData}
+        //   filled
+        //   pickable
+        //   getHexagons={e => e.h3}
+        //   getFillColor={[0, 128, 200]}
+        // /> */}
+        //   <Map mapboxAccessToken="pk.eyJ1IjoiZ2hlcm1hbnQiLCJhIjoiY2pncDUwcnRmNDQ4ZjJ4czdjZXMzaHZpNyJ9.3rFyYRRtvLUngHm027HZ7A" />
+        // </DeckGL>
+      }
+    </AppShell>
+
   );
 }
